d0e3ffde94109d853816a46e453d79328850faa3
Darren Luo <darrenluo1993@163.com>
2024-07-30 14:11:53 +0800

    C语言预处理器学习

--- src/preprocessor/operator/defined.c
+++ src/preprocessor/operator/defined.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
 
-#if !defined(MESSAGE)
+#if !defined MESSAGE
 #define MESSAGE "You wish!"
+#endif
+
+#if defined MESSAGE
+#undef MESSAGE
+#define MESSAGE "You wish new!"
+#endif
+
+#if !defined (MESSAGE1)
+#define MESSAGE1 "You wish 1!"
+#endif
+
+#if defined (MESSAGE1)
+#undef MESSAGE1
+#define MESSAGE1 "You wish 1 new!"
+#endif
+
+#ifndef MESSAGE2
+#define MESSAGE2 "You wish 2!"
+#endif
+
+#ifdef MESSAGE2
+#undef MESSAGE2
+#define MESSAGE2 "You wish 2 new!"
 #endif
 
 int main(void) {
     printf("Here is the message: %s\n", MESSAGE);
+    printf("Here is the message1: %s\n", MESSAGE1);
+    printf("Here is the message2: %s\n", MESSAGE2);
     return 0;
 }
